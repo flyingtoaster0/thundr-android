162653057f3157d286fef856f0dcc2565fef8ca4
flyingtoaster0 <flyingtoaster0@gmail.com>
2014-10-07 23:00:50 -0400

    Implemented saving courses

--- app/src/main/java/com/flyingtoaster/thundr/MyCoursesFragment.java
+++ app/src/main/java/com/flyingtoaster/thundr/MyCoursesFragment.java
@@ -5,12 +5,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 /**
  * Created by tim on 10/6/14.
  */
 public class MyCoursesFragment extends Fragment {
     View mRootView;
+    ListView mMyCoursesListView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,9 @@ public class MyCoursesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mRootView = inflater.inflate(R.layout.fragment_my_courses, container, false);
+
+        mMyCoursesListView = (ListView)mRootView.findViewById(R.id.my_courses_listview);
+
         return mRootView;
     }
 }
