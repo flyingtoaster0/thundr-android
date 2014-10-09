5f78f74fb87634e3bc321e661ca858637a31e89e
flyingtoaster0 <flyingtoaster0@gmail.com>
2014-10-09 01:18:42 -0400

    Robolectric tests output xml! :D

--- app/src/androidTest/java/com/flyingtoaster/thundr/MainActivityTest.java
+++ app/src/androidTest/java/com/flyingtoaster/thundr/MainActivityTest.java
@@ -1,33 +1,24 @@
 package com.flyingtoaster.thundr;
 
-import org.robolectric.Robolectric;
-import org.robolectric.RobolectricTestRunner;
 import static org.hamcrest.CoreMatchers.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.util.ActivityController;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
 
-import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.*;
 
 @RunWith(RobolectricTestRunner.class)
 public class MainActivityTest {
     @Test
     public void shouldHaveApplicationName() throws Exception {
-        //String appName = new MainActivity().getResources().getString(R.string.app_name);
         ActivityController controller = Robolectric.buildActivity(MainActivity.class).create().start();
-
         MainActivity activity = (MainActivity)controller.get();
         assertThat(activity.getSlidingMenu(), notNullValue());
-
         activity.mOpenMenuButton.callOnClick();
 
         assertThat(activity.getSlidingMenu().isMenuShowing(), is(true));
-        //activity.mOpenMenuButton.callOnClick();
-
-        //Thread.sleep(1000);
-
-        //assertThat(activity.getSlidingMenu().isMenuShowing(), equalTo(true));
     }
 
     @Test
